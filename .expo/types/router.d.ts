10ca8f85dad684a6864d8fbcfd5e0f6c21f6fd66
AnuSD <anuradhabelkune09@gmail.com>
2025-08-25 18:44:47 +0530

    Initial commit - WhatsApp Clone React Native

---
+++ .expo/types/router.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import * as Router from 'expo-router';
+
+export * from 'expo-router';
+
+declare module 'expo-router' {
+  export namespace ExpoRouter {
+    export interface __routes<T extends string | object = string> {
+      hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}` | `/`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}/login` | `/login`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}/terms_agree` | `/terms_agree`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}/verify_otp` | `/verify_otp`; params?: Router.UnknownInputParams; } | { pathname: `${'/(main)'}` | `/`; params?: Router.UnknownInputParams; };
+      hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(auth)'}` | `/`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(auth)'}/login` | `/login`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(auth)'}/terms_agree` | `/terms_agree`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(auth)'}/verify_otp` | `/verify_otp`; params?: Router.UnknownOutputParams; } | { pathname: `${'/(main)'}` | `/`; params?: Router.UnknownOutputParams; };
+      href: Router.RelativePathString | Router.ExternalPathString | `/_sitemap${`?${string}` | `#${string}` | ''}` | `${'/(auth)'}${`?${string}` | `#${string}` | ''}` | `/${`?${string}` | `#${string}` | ''}` | `${'/(auth)'}/login${`?${string}` | `#${string}` | ''}` | `/login${`?${string}` | `#${string}` | ''}` | `${'/(auth)'}/terms_agree${`?${string}` | `#${string}` | ''}` | `/terms_agree${`?${string}` | `#${string}` | ''}` | `${'/(auth)'}/verify_otp${`?${string}` | `#${string}` | ''}` | `/verify_otp${`?${string}` | `#${string}` | ''}` | `${'/(main)'}${`?${string}` | `#${string}` | ''}` | `/${`?${string}` | `#${string}` | ''}` | { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}` | `/`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}/login` | `/login`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}/terms_agree` | `/terms_agree`; params?: Router.UnknownInputParams; } | { pathname: `${'/(auth)'}/verify_otp` | `/verify_otp`; params?: Router.UnknownInputParams; } | { pathname: `${'/(main)'}` | `/`; params?: Router.UnknownInputParams; };
+    }
+  }
+}
